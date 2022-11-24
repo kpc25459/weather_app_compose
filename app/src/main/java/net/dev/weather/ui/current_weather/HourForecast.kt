ac09ade2bce7811781060c7bea60d48b074a9576
kpc25459 <kpc25459@gmail.com>
2022-11-24 20:17:42 +0100

    fix hour forecast row

--- app/src/main/java/net/dev/weather/ui/current_weather/HourForecast.kt
+++ app/src/main/java/net/dev/weather/ui/current_weather/HourForecast.kt
@@ -2,7 +2,7 @@ package net.dev.weather.ui.current_weather
 
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Icon
@@ -14,7 +14,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.datetime.Instant
@@ -28,20 +27,13 @@ import kotlin.math.roundToInt
 @Composable
 fun HourForecast(currentWeather: CurrentWeatherViewModel) {
 
-    val scrollState = rememberScrollState()
-
     val hourlyForecast by currentWeather.hourlyForecast.observeAsState()
     val timeZone by currentWeather.timeZone.observeAsState()
 
     hourlyForecast?.let { forecast ->
-        Row(Modifier.horizontalScroll(scrollState)) {
-            LazyColumn(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.padding(10.dp)
-            ) {
-                items(forecast) { item ->
-                    timeZone?.let { HourForecastItem(item, it) }
-                }
+        LazyRow(/*modifier = Modifier.horizontalScroll(rememberScrollState())*/) {
+            items(forecast) { item ->
+                timeZone?.let { HourForecastItem(item, it) }
             }
         }
     }
@@ -54,10 +46,13 @@ fun HourForecastItem(item: WeatherHourly, timeZone: TimeZone) {
 
     val dayOfWeek = "${dateTime.date.dayOfWeek.toString().lowercase(Locale.getDefault()).substring(0, 3).replaceFirstChar { it.uppercase() }}."
 
-    Text(text = dayOfWeek)
-    Icon(imageVector = Icons.Filled.Close, contentDescription = null)
-    Text(text = dateTime.time.toString())
-    Text(text = "${item.temp.roundToInt()}°")
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(10.dp)) {
+        Text(text = dayOfWeek)
+        Icon(imageVector = Icons.Filled.Close, contentDescription = null)
+        Text(text = dateTime.time.toString())
+        Text(text = "${item.temp.roundToInt()}°")
+    }
+
 }
 
 @Preview(showBackground = true)
