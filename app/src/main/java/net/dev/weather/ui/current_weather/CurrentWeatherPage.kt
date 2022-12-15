8b23d0890aa87f3385be307db9d1e375bb02e1a8
kpc25459 <kpc25459@gmail.com>
2022-12-15 21:25:22 +0100

    feat add scroll

--- app/src/main/java/net/dev/weather/ui/current_weather/CurrentWeatherPage.kt
+++ app/src/main/java/net/dev/weather/ui/current_weather/CurrentWeatherPage.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,7 +21,7 @@ fun CurrentWeatherPage() {
 
     val viewModel = CurrentWeatherViewModel(repository)
 
-    Column(modifier = Modifier.padding(5.dp)) {
+    Column(modifier = Modifier.padding(5.dp).verticalScroll(rememberScrollState())) {
         Box(viewModel)
         Spacer(modifier = Modifier.height(20.dp))
         HourForecast(viewModel)
