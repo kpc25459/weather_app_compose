41ddc0916fd374b174e194e679e475950e8ce835
kpc25459 <kpc25459@gmail.com>
2024-01-07 11:21:45 +0100

    refactor add BuildConfig props

--- app/src/main/java/net/dev/weather/network/api/WeatherServiceApi.kt
+++ app/src/main/java/net/dev/weather/network/api/WeatherServiceApi.kt
@@ -1,5 +1,6 @@
 package net.dev.weather.network.api
 
+import net.dev.weather.BuildConfig
 import net.dev.weather.network.model.AirPollutionResponse
 import net.dev.weather.network.model.CurrentLocation
 import net.dev.weather.network.model.OneCallResponse
@@ -14,7 +15,7 @@ interface WeatherServiceApi {
         @Query("lat") latitude: Double,
         @Query("lon") longitude: Double,
         @Query("limit") limit: Int = 1,
-        @Query("appid") appid: String = Companion.appid,
+        @Query("appid") appid: String = BuildConfig.OPEN_WEATHER_API_KEY,
     ): Response<List<CurrentLocation>>
 
     @GET("/data/2.5/onecall")
@@ -24,25 +25,24 @@ interface WeatherServiceApi {
         @Query("exclude") exclude: String = "alerts",
         @Query("units") units: String = "metric",
         @Query("lang") lang: String = "pl",
-        @Query("appid") appid: String = Companion.appid,
+        @Query("appid") appid: String = BuildConfig.OPEN_WEATHER_API_KEY,
     ): Response<OneCallResponse>
 
     @GET("/data/2.5/air_pollution")
     suspend fun getAirPollution(
         @Query("lat") latitude: Double,
         @Query("lon") longitude: Double,
-        @Query("appid") appid: String = Companion.appid,
+        @Query("appid") appid: String = BuildConfig.OPEN_WEATHER_API_KEY,
     ): Response<AirPollutionResponse>
 
     @GET("/data/2.5/air_pollution/forecast")
     suspend fun getAirPollutionForecast(
         @Query("lat") latitude: Double,
         @Query("lon") longitude: Double,
-        @Query("appid") appid: String = Companion.appid,
+        @Query("appid") appid: String = BuildConfig.OPEN_WEATHER_API_KEY,
     ): Response<AirPollutionResponse>
 
     companion object {
         const val BASE_URL = "http://api.openweathermap.org/"
-        private val appid = System.getenv("OPEN_WEATHER_API_KEY")!!
     }
 }
